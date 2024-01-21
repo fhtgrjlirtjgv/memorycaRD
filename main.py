8f36fb7cf573f991928b401431767fb19491fd1f
fhtgrjlirtjgv <154062090+fhtgrjlirtjgv@users.noreply.github.com>
2024-01-21 17:32:37 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout
 from random import choice, shuffle
+from time import sleep
 app = QApplication([]) #сторюємо віконний додаток
 
 from window import*
-
+from menu import*
 
 class Question():
     current = None
@@ -19,10 +20,10 @@ class Question():
 
 question = [
     Question("вісім", "eight", "eit", "eidhgt", "nine"),
-    Question("чотири", "foure", "fove", "fare", "four"),
-    Question("три", "tri", "three", "try", "troy"),
-    Question("один", "odun", "oni", "onene", "one"),
-    Question("два", "thoo", "the", "thy", "two"),
+    Question("чотири", "four", "fove", "fare", "foure"),
+    Question("три", "three", "tri", "try", "troy"),
+    Question("один", "one", "oni", "onene", "odun"),
+    Question("два", "two", "the", "thy", "thoo"),
     ]
 
 radio_list = [btn1, btn2, btn3, btn4]
@@ -33,6 +34,7 @@ win.setWindowTitle("Memory Card")
 win.setLayout(main_line)
 
 def next_question():
+    
     Question.current = choice(question)
     question_lb.setText(Question.current.text)
     shuffle(radio_list)
@@ -42,19 +44,49 @@ def next_question():
     radio_list[3].setText(Question.current.ans4)
 
 
+def check_answer():
+    if radio_list[0].isChecked():
+        result_text.setText("Правильно")
+    else:
+        result_text.setText("Неправильно")
+
+    radio_group.setExclusive(False)
+    for btn in radio_list:
+        btn.setChecked(False)
+    radio_group.setExclusive(True)
 
 def answer_click():
     if answer_btn.text() == "Відповісти":
-        group_box.hide()
-        result_box.show()
-        answer_btn.setText("Наступне питання")
+        if radio_group.checkedButton():
+            check_answer()
+            group_box.hide()
+            result_box.show()
+            answer_btn.setText("Наступне питання")
     else:
         next_question()
         group_box.show()
         result_box.hide()
-        answer_btn.setText("Наступне питання")
+        answer_btn.setText("Відповісти")
+        
+def show_menu():
+    win.hide()
+    menu_win.show()
+
+def hide_menu():
+    win.show()
+    menu_win.hide()
+
+def relax():
+    pause_time = int(time_spin.value()) * 60
+    win.hide()
+    sleep(pause_time)
+    win.show()
+
 
 answer_btn.clicked.connect(answer_click)
+menu_btn.clicked.connect(show_menu)
+back_btn.clicked.connect(hide_menu)
+rest_btn.clicked.connect(relax)
 
 next_question()
 # вкінці
